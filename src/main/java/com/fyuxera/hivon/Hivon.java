8d17d975109b24eea558d52664303fe31bc7e6e5
sanodmendis <sanodmendis@gmail.com>
2026-01-23 23:40:18 +0530

    init

---
+++ src/main/java/com/fyuxera/hivon/Hivon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fyuxera.hivon;
+
+/**
+ *
+ * @author Sanod D. Mendis
+ */
+public class Hivon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/reports/analytics/DailyActivityReport.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reports.analytics;
+
+/**
+ *
+ * @author Sanod
+ */
+public class DailyActivityReport {
+    
+}
